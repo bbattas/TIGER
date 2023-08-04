1a8811937b4d60afd94a06bc8717954b12ff24ef
bbattas <bbdumpster@gmail.com>
2023-08-04 16:27:29 -0400

    Working image analysis curvature test

--- TEMP_imageCurvature.py
+++ TEMP_imageCurvature.py
@@ -13,7 +13,7 @@ verb = logging.info
 # from VolumeScripts import *
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+from matplotlib.patches import Polygon, Circle
 from matplotlib.collections import PolyCollection
 from matplotlib.collections import PatchCollection
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -34,6 +34,65 @@ import cv2
 from scipy.interpolate import splprep, splev
 
 
+# def find_circle(self,A,B,C):
+#     a = A[1]-B[1]
+#     b = A[0]-B[0]
+#     c = A[1]-C[1]
+#     d = A[0]-C[0]
+#     e = ((A[1]**2-B[1]**2)-(B[0]**2-A[0]**2))/2.0
+#     f = ((A[1]**2-C[1]**2)-(C[0]**2-A[0]**2))/2.0
+#     x0 = -(d*e-b*f)/(b*c-a*d)
+#     y0 = -(a*f-c*e)/(b*c-a*d)
+#     r = math.sqrt((A[1]-x0)**2+(A[0]-y0)**2)
+#     return x0,y0,r
+
+# def check_linear(pt0,pt1,pt2):
+#     return
+# fig, ax = plt.subplots()
+#     plt.scatter(*a,c='red')
+#     plt.scatter(*b,c='black')
+#     plt.scatter(*c,c='green')
+#     cir = plt.Circle((x_ctr,y_ctr),r)
+#     ax.add_patch(cir)
+#     plt.show()
+
+def find_circle(a,b,c,fullcontour,returnXY=False):
+    # Using the process from http://paulbourke.net/geometry/circlesphere/
+    # slope of ab and bc
+    tol = 1e-8
+    if b[0] == a[0] and b[0] == c[0]:
+        # Vertical line
+        print('vertical')
+        if returnXY:
+            return -1, 0, 0, 0
+        else:
+            return -1, 0
+    # Calculate Slopes between the points
+    m1 = (b[1] - a[1])/(b[0] - a[0])
+    m2 = (c[1] - b[1])/(c[0] - b[0])
+    if m2 == m1:
+        # Horizontal line
+        if returnXY:
+            return -1, 0, 0, 0
+        else:
+            return -1, 0
+    # Correction for when 2 points are stacked vertically to prevent the slope = nan
+    if b[0] == a[0]:
+        m1 = (b[1] - a[1])/(tol)
+    if b[0] == c[0]:
+        m2 = (c[1] - b[1])/(tol)
+    # Calculate the center points based on where the lines perpendicular cross
+    x_ctr = (m1*m2*(a[1] - c[1]) + m2*(a[0] + b[0]) - m1*(b[0] + c[0]))/(2*(m2 - m1))
+    # y_ctr = -(1/m1)*(x_ctr - ((a[0] + b[0])/2) ) + ((a[1] + b[1])/2)
+    y_ctr = (m1*(a[1] + b[1]) - m2*(b[1] + c[1]) + (a[0] - c[0]))/(2*(m1-m2))
+    r = math.sqrt((a[0] - x_ctr)**2 + (a[1] - y_ctr)**2)
+    # this returns -1 when the point is outside the contour, 1 inside and 0 on it
+    signage = cv2.pointPolygonTest(fullcontour, (x_ctr,y_ctr), False)
+    if returnXY:
+        return r, signage, x_ctr, y_ctr
+    else:
+        return r, signage
+
 def scaleFactor(bw_img_w_box,xrange,yrange=None):
     # Pixels go 0 -> n for x but y is 0 v n down (0,0 is top left)
     # https://pyimagesearch.com/2021/01/20/opencv-getting-and-setting-pixels/
@@ -53,19 +112,41 @@ def scaleFactor(bw_img_w_box,xrange,yrange=None):
     # cv2.imwrite('scaling_domain_contour.jpg', src)
     return xrange/x_pixels, min(x[0]), min(y[0])
 
-def curvature_fromImage(bw_img_w_box,xrange):
+def curvature_fromImage(bw_img_w_box,xrange,nn):
     scale, xmin, ymin = scaleFactor(bw_img_w_box,xrange)
     src = cv2.imread(bw_img_w_box)#,-1
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)  # convert to grayscale
     blur = cv2.medianBlur(gray,45)
     ret, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)#CHAIN_APPROX_SIMPLE)
     n = 0
-    print(contours)
+    # print(contours)
     if 0 in contours[n]:
         n += 1
     x,y = contours[n].T
-    return
+    if xmin in x[0] or ymin in y[0]:
+        print("Plot Boundary in second contour, using third")
+        n +=1
+        x,y = contours[n].T
+    xy = [np.asarray([a,b]) for (a,b) in zip(x[0],y[0])]
+    rad_loc = []
+    for i in range(len(xy)):
+        if (i + nn) > (len(xy) - 1):
+            rad_loc.append(find_circle(xy[i-nn],xy[i],xy[i+nn-len(xy)],contours[n]))
+        else:
+            rad_loc.append(find_circle(xy[i-nn],xy[i],xy[i+nn],contours[n]))
+    print(rad_loc)
+    radii = [row[0] for row in rad_loc]
+    sign = [row[1] for row in rad_loc]
+    curvature = 1/(np.asarray(radii)*scale)
+    curvature = np.where(np.asarray(radii)==-1,0,-1*np.asarray(sign)*curvature)
+    print(np.average(curvature))
+    # # Testing Plot
+    cm = plt.cm.get_cmap('RdYlBu')
+    sc = plt.scatter((x-xmin)*scale, (y-ymin)*scale, c=curvature, vmin=min(curvature), vmax=max(curvature), s=35, cmap=cm)
+    plt.colorbar(sc)
+    plt.show()
+    return curvature
 
 if __name__ == "__main__":
     print("__main__ Start")
@@ -74,7 +155,11 @@ if __name__ == "__main__":
     print(xcoord[1])
     print(xcoord[1-3])
     scaleFactor('02_3grain_base_cv2_bwbox_gb_x250_test_60.png',300)
-    curvature_fromImage('02_3grain_base_cv2_bwbox_gb_x250_test_60.png',300)
+    read_ti = time.perf_counter()
+    # curvature_fromImage('02_3grain_base_cv2_bwbox_gb_x250_test_60.png',300,3)
+    curvature_fromImage('02_2grain_full_test_cv2_bwbox_gb_x250_test_60.png',300,3)
+    read_tf = time.perf_counter()
+    print("  Finished testing:",round(read_tf-read_ti,2),"s")
     # print(xcoord[4+2])
     # image = cv2.imread('02_3grain_base_sliced_x250_delta_gr0_test_60.png')
     # img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -99,6 +184,13 @@ if __name__ == "__main__":
     # print(conBox)
     # Finding contours for the thresholded image #im2,
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # create hull array for convex hull points
+    hull = []
+
+    # calculate points for each contour
+    for i in range(len(contours)):
+        # creating convex hull object for each contour
+        hull.append(cv2.convexHull(contours[i], False))
     # print(contours)
     # smoothened = []
     # for contour in contours:
@@ -126,7 +218,7 @@ if __name__ == "__main__":
         # # Overlay the smoothed contours on the original image
         # cv2.drawContours(drawing, smoothened, -1, (255,255,255), 2)
         # draw ith convex hull object
-        # cv2.drawContours(drawing, hull, i, color, 1, 8)
+        cv2.drawContours(drawing, hull, i, color, 1, 8)
     # cv2.imshow('drawing', drawing)
     # cv2.waitKey(0)
     cv2.imwrite('image_contours_src.jpg', src)
