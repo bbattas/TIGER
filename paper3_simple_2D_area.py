2814c005602c3732b823f74e9a1453608bee7381
bbattas <bbdumpster@gmail.com>
2024-09-23 15:51:28 -0400

    correction to account for seq i think?

--- paper3_simple_2D_area.py
+++ paper3_simple_2D_area.py
@@ -494,7 +494,7 @@ def para_volume_calc(time_step,i,t_frames,dimension_case,op_max):
         ValueError: If elemental calculations are not set up or the dimension case is unknown.
     """
     db("Calculating frame",i+1)
-    x,y,z,c = MF.get_data_at_time(cl_args.var,t_frames[time_step])
+    x,y,z,c = MF.get_data_at_time(cl_args.var,t_frames[i])#time_step
     match dimension_case:
         case DimensionCase.D2_NODAL:
             # db("Processing 2D Nodal case")
